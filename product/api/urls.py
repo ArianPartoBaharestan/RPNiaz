b02fac07b40ae2997a6dae22cfb57307b7a150bd
rbabaeii <you@example.com>
2023-01-10 00:38:46 +0330

    fix bugs

--- product/api/urls.py
+++ product/api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list-active/',  views.ListActiveProductsView.as_view() , name = 'active-products'),
     path('list-all/',  views.ListAllProductsView.as_view() , name = 'all-products'),
     path('create/' , views.CreateProductView.as_view() , name = "create"),
-    path('detail/<int:pk>/' , views.DetailProductView.as_view() , name = "detail"),
+    path('detail/<str:pk>/' , views.DetailProductView.as_view() , name = "detail"),
     path('user/active-products/' , views.ListUserActiveProducts.as_view()),
     path('user/waiting-products/' , views.ListUserWaitingProducts.as_view()),
 ]
