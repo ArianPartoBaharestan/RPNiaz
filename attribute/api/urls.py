d22b2a90b0e4f74100e58c4c505a93ab060b7824
niluws <ns1378@yahoo.com>
2023-01-13 22:46:12 +0330

    attr

--- attribute/api/urls.py
+++ attribute/api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from attribute.api import views
 
 urlpatterns = [
-    path('AttributeGroup/', views.AttributeGroupView.as_view()),
-    path('Attribute/', views.AttributeView.as_view()),
-    path('AttributeItem/', views.AttributeItemView.as_view()),
-    path('ProductAttribute/', views.ProductAttributeView.as_view()),
+    path('AttributeGroup/<str:pk>/', views.AttributeGroupView.as_view()),
+    path('Attribute/<str:pk>/', views.AttributeView.as_view()),
+    path('AttributeItem/<str:pk>/', views.AttributeItemView.as_view()),
+    path('ProductAttribute/attrs/<str:pk>/', views.ProductAttributeView.as_view()),
+    path('ProductAttribute/create/',views.CreateProductAttributeView.as_view()),
+
+
 ]
